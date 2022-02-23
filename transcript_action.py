940449726a1f19b7d3ee488878291a6a38dc3e7b
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2022-02-23 21:21:17 +0100

    get added files

--- transcript_action.py
+++ transcript_action.py
@@ -6,19 +6,13 @@ import subprocess
 
 from transcript import print_timecodes
 
-
 if __name__ == '__main__':
-    sha = sys.argv[1]
-    command = ['git', 'diff', '--name-only', sha, f'{sha}^1']
+    input_file = sys.argv[1]
 
-    changed_files = subprocess.check_output(command).decode()
-    input_files = changed_files.split()
+    output_file = input_file \
+        .replace('docs/', 'transcripts/') \
+        .replace('.docx', '.txt')
 
-    for input_file in input_files:
-        output_file = input_file \
-            .replace('docs/', 'transcripts/') \
-            .replace('.docx', '.txt')
+    print(f'processing {input_file} -> {output_file}')
 
-        print(f'processing {input_file} -> {output_file}')
-
-        print_timecodes(input_file, output_file)
+    print_timecodes(input_file, output_file)
