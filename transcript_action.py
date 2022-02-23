0e9aa6c41fb18058da248109f168d6f956ddfc7d
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2022-02-23 21:22:51 +0100

    fix

--- transcript_action.py
+++ transcript_action.py
@@ -6,8 +6,10 @@ import subprocess
 
 from transcript import print_timecodes
 
-if __name__ == '__main__':
-    input_file = sys.argv[1]
+def main(input_file):
+    if not input_file.endswith('.docx'):
+        print('not a docx file')
+        return 
 
     output_file = input_file \
         .replace('docs/', 'transcripts/') \
@@ -16,3 +18,8 @@ if __name__ == '__main__':
     print(f'processing {input_file} -> {output_file}')
 
     print_timecodes(input_file, output_file)
+
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    main(input_file)
